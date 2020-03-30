35cb6783bb81fc4a3a522de09811df4c8b582567
Haryz Izzudin <haryzizzudin@gmail.com>
2020-03-30 23:52:38 +0800

    Add database reset functionality

--- src/EduPartner.MvcApp/Data/SeedData.cs
+++ src/EduPartner.MvcApp/Data/SeedData.cs
@@ -15,90 +15,102 @@ namespace EduPartner.MvcApp.Data
         {
             using (var context = serviceProvider.GetRequiredService<EduPartnerDbContext>())
             {
-                var parent = new Parent
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Ali bin Abu",
-                    Email = "ali@test.com",
-                    Password = "abc123",
-                    Address = "1, Jalan 2, 40000 Shah Alam, Selangor"
-                };
-
-                context.Parents.Add(parent);
-
-                var childOne = new Child
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Ahmad bin Ali",
-                    DateOfBirth = new DateTime(2013, 1, 1),
-                    Parent = parent
-                };
-
-                var childTwo = new Child
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Aishah binti Ali",
-                    DateOfBirth = new DateTime(2010, 1, 1),
-                    Parent = parent
-                };
-
-                context.Children.AddRange(childOne, childTwo);
-
-                var teacherOne = new Teacher
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Fatimah binti Zaid",
-                    PhoneNumber = "60192223333"
-                };
-
-                var teacherTwo = new Teacher
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Abdul Aziz bin Rahim",
-                    PhoneNumber = "60123334444"
-                };
-
-                var teacherThree = new Teacher
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Siti Hawa binti Aiman",
-                    PhoneNumber = "60164445555"
-                };
-
-                context.Teachers.AddRange(teacherOne, teacherTwo, teacherThree);
-
-                var mathSubject = new Subject
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Mathematics",
-                    MonthlyFee = 150.00m,
-                    Teachers = new List<Teacher> { teacherOne }
-                };
-
-                var scienceSubject = new Subject
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Science",
-                    MonthlyFee = 170.00m,
-                    Teachers = new List<Teacher> { teacherTwo, teacherThree }
-                };
-
-                context.Subjects.AddRange(mathSubject, scienceSubject);
-
-                var enrollment = new Enrollment
-                {
-                    Id = Guid.NewGuid(),
-                    Child = childOne,
-                    Subject = mathSubject,
-                    Teacher = teacherOne,
-                    TimeslotDayOfWeek = DayOfWeek.Monday,
-                    TimeslotTime = new DateTime(2020, 1, 1, 14, 0, 0)
-                };
-
-                context.Enrollments.Add(enrollment);
-
-                context.SaveChanges();
+                InitializeContext(context);
             }
+        }
+
+        public static void ResetContext(EduPartnerDbContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            InitializeContext(context);
+        }
+
+        private static void InitializeContext(EduPartnerDbContext context)
+        {
+            var parent = new Parent
+            {
+                Id = Guid.NewGuid(),
+                Name = "Ali bin Abu",
+                Email = "ali@test.com",
+                Password = "abc123",
+                Address = "1, Jalan 2, 40000 Shah Alam, Selangor"
+            };
+
+            context.Parents.Add(parent);
+
+            var childOne = new Child
+            {
+                Id = Guid.NewGuid(),
+                Name = "Ahmad bin Ali",
+                DateOfBirth = new DateTime(2013, 1, 1),
+                Parent = parent
+            };
+
+            var childTwo = new Child
+            {
+                Id = Guid.NewGuid(),
+                Name = "Aishah binti Ali",
+                DateOfBirth = new DateTime(2010, 1, 1),
+                Parent = parent
+            };
+
+            context.Children.AddRange(childOne, childTwo);
+
+            var teacherOne = new Teacher
+            {
+                Id = Guid.NewGuid(),
+                Name = "Fatimah binti Zaid",
+                PhoneNumber = "60192223333"
+            };
+
+            var teacherTwo = new Teacher
+            {
+                Id = Guid.NewGuid(),
+                Name = "Abdul Aziz bin Rahim",
+                PhoneNumber = "60123334444"
+            };
+
+            var teacherThree = new Teacher
+            {
+                Id = Guid.NewGuid(),
+                Name = "Siti Hawa binti Aiman",
+                PhoneNumber = "60164445555"
+            };
+
+            context.Teachers.AddRange(teacherOne, teacherTwo, teacherThree);
+
+            var mathSubject = new Subject
+            {
+                Id = Guid.NewGuid(),
+                Name = "Mathematics",
+                MonthlyFee = 150.00m,
+                Teachers = new List<Teacher> { teacherOne }
+            };
+
+            var scienceSubject = new Subject
+            {
+                Id = Guid.NewGuid(),
+                Name = "Science",
+                MonthlyFee = 170.00m,
+                Teachers = new List<Teacher> { teacherTwo, teacherThree }
+            };
+
+            context.Subjects.AddRange(mathSubject, scienceSubject);
+
+            var enrollment = new Enrollment
+            {
+                Id = Guid.NewGuid(),
+                Child = childOne,
+                Subject = mathSubject,
+                Teacher = teacherOne,
+                TimeslotDayOfWeek = DayOfWeek.Monday,
+                TimeslotTime = new DateTime(2020, 1, 1, 14, 0, 0)
+            };
+
+            context.Enrollments.Add(enrollment);
+
+            context.SaveChanges();
         }
     }
 }
