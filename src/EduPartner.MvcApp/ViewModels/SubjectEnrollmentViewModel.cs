f10815c32955d36c6a71c3bfe3c78376ea986b45
Haryz Izzudin <haryzizzudin@gmail.com>
2020-03-31 18:05:19 +0800

    Add subject enrollment functionality

---
+++ src/EduPartner.MvcApp/ViewModels/SubjectEnrollmentViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EduPartner.MvcApp.ViewModels
+{
+    public class SubjectEnrollmentViewModel
+    {
+        public Guid ChildId { get; set; }
+        public Guid SubjectId { get; set; }
+        public Guid TeacherId { get; set; }
+        public DayOfWeek TimeslotDayOfWeek { get; set; }
+        public DateTime TimeslotTime { get; set; }
+        public bool IsHomeTutoring { get; set; }
+    }
+}
