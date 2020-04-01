211c2a6985ed5a67e4a8aac35ae3f0c5c214df98
Haryz Izzudin <haryzizzudin@gmail.com>
2020-04-01 17:53:29 +0800

    Implement upcoming classes on dashboard

--- src/EduPartner.MvcApp/Controllers/HomeController.cs
+++ src/EduPartner.MvcApp/Controllers/HomeController.cs
@@ -6,16 +6,22 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using EduPartner.MvcApp.ViewModels;
+using EduPartner.MvcApp.Data;
+using Microsoft.EntityFrameworkCore;
+using NodaTime;
+using System.Globalization;
 
 namespace EduPartner.MvcApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly EduPartnerDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, EduPartnerDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -28,8 +34,39 @@ namespace EduPartner.MvcApp.Controllers
             return View();
         }
 
-        public IActionResult Dashboard()
+        public async Task<IActionResult> Dashboard()
         {
+            var enrollments = await _context.Enrollments
+                .Include(e => e.Child)
+                .Include(e => e.Subject)
+                .Include(e => e.Teacher)
+                .ToListAsync();
+
+            var events = new List<DashboardUpcomingEventViewModel>();
+            var startDateTime = DateTimeOffset.Now;
+
+            foreach (var enrollment in enrollments)
+            {
+                int daysUntilNextOccurrence = ((int)enrollment.TimeslotDayOfWeek - (int)startDateTime.DayOfWeek + 7) % 7;
+                var nextOccurrence = startDateTime.AddDays(daysUntilNextOccurrence);
+
+                nextOccurrence = new DateTimeOffset(nextOccurrence.Date + new TimeSpan(enrollment.TimeslotTime.Hour, enrollment.TimeslotTime.Minute, 0), nextOccurrence.Offset);
+                int idCounter = 1;
+
+                events.Add(new DashboardUpcomingEventViewModel
+                {
+                    ChildName = enrollment.Child.Name,
+                    SubjectName = enrollment.Subject.Name,
+                    StartTimestamp = nextOccurrence,
+                    IsHomeTutoring = enrollment.IsHomeTutoring
+                });
+
+                nextOccurrence = nextOccurrence.AddDays(7);
+                idCounter++;
+            }
+
+            ViewData["Events"] = events.OrderBy(e => e.StartTimestamp).ToList();
+
             return View();
         }
 
